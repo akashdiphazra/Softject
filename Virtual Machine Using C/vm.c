d926056f451f07516bce47616593eae543a4c49a
akashdiphazra <princehazra.96@gmail.com>
2022-12-15 19:12:44 +0530

    Errors fixed and new instructions added in virtual machine

--- Virtual Machine Using C/vm.c
+++ Virtual Machine Using C/vm.c
@@ -1,4 +1,3 @@
-// https://youtu.be/0irYsCYuZws?list=PLpM-Dvs8t0VY73ytTCQqgvgCWttV3m8LM&t=3138
 #include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -6,11 +5,26 @@
 #define VM_STACK_CAPACITY 1024
 
 typedef enum {
-  TRAP_OK = 0,
-  TRAP_STACK_OVERFLOW,
-  TRAP_STACK_UNDERFOLOW,
-  TRAP_ILLEGAL_INSTRUCTION
+  ERR_OK = 0,
+  ERR_STACK_OVERFLOW,
+  ERR_STACK_UNDERFOLOW,
+  ERR_ILLEGAL_INSTRUCTION
 } Trap;
+
+const char *trap_as_cstr(Trap trap) {
+  switch (trap) {
+  case ERR_OK:
+    return "ERR_OK";
+  case ERR_STACK_OVERFLOW:
+    return "ERR_STACK_OVERFLOW";
+  case ERR_STACK_UNDERFOLOW:
+    return "ERR_STACK_UNDERFOLOW";
+  case ERR_ILLEGAL_INSTRUCTION:
+    return "ERR_ILLEGAL_INSTRUCTION";
+  default:
+    assert(0 && "trap_as_cstr: UNREACHABLE");
+  }
+}
 
 typedef int64_t Word;
 typedef struct {
@@ -18,60 +32,95 @@ typedef struct {
   size_t stack_size;
 } virtualmachine;
 
-typedef enum {
-  INST_PUSH,
-  INST_PLUS,
-} Inst_Type;
+typedef enum { INST_PUSH, INST_PLUS, INST_MINUS, INST_MUL, INST_DIV } Inst_Type;
 
 typedef struct {
   Inst_Type type;
   Word operand;
 } Inst;
 
-Inst inst_push(Word operand) {
-  return (Inst){.type = INST_PUSH, .operand = operand};
-}
-
-Inst inst_plus(void) { return (Inst){.type = INST_PLUS}; }
+#define MAKE_INST_PUSH(value)                                                  \
+  { .type = INST_PUSH, .operand = (value) }
+#define MAKE_INST_PLUS                                                         \
+  { .type = INST_PLUS }
+#define MAKE_INST_MINUS                                                        \
+  { .type = INST_MINUS }
+#define MAKE_INST_MUL                                                          \
+  { .type = INST_MUL }
+#define MAKE_INST_DIV                                                          \
+  { .type = INST_DIV }
 
 Trap vm_execute_inst(virtualmachine *vm, Inst inst) {
   switch (inst.type) {
   case INST_PUSH:
     if (vm->stack_size >= VM_STACK_CAPACITY) {
-      return TRAP_STACK_UNDERFOLOW;
+      return ERR_STACK_UNDERFOLOW;
     }
     vm->stack[vm->stack_size++] = inst.operand;
     break;
   case INST_PLUS:
     if (vm->stack_size < 2) {
-      return TRAP_STACK_OVERFLOW;
+      return ERR_STACK_OVERFLOW;
+    } else {
+      vm->stack[vm->stack_size - 2] += vm->stack[vm->stack_size - 1];
+      vm->stack_size -= 1;
     }
-    vm->stack[vm->stack_size - 2] += vm->stack[vm->stack_size - 1];
-    vm->stack_size -= 1;
+    break;
+  case INST_MINUS:
+    if (vm->stack_size < 2) {
+      return ERR_STACK_OVERFLOW;
+    } else {
+      vm->stack[vm->stack_size - 2] -= vm->stack[vm->stack_size - 1];
+      vm->stack_size -= 1;
+    }
+    break;
+  case INST_MUL:
+    if (vm->stack_size < 2) {
+      return ERR_STACK_OVERFLOW;
+    } else {
+      vm->stack[vm->stack_size - 2] *= vm->stack[vm->stack_size - 1];
+      vm->stack_size -= 1;
+    }
+    break;
+  case INST_DIV:
+    if (vm->stack_size < 2) {
+      return ERR_STACK_OVERFLOW;
+    } else {
+      vm->stack[vm->stack_size - 2] /= vm->stack[vm->stack_size - 1];
+      vm->stack_size -= 1;
+    }
     break;
   default:
-    return TRAP_ILLEGAL_INSTRUCTION;
+    return ERR_ILLEGAL_INSTRUCTION;
   }
-  return TRAP_OK;
+  return ERR_OK;
 }
 
-void vm_dump(const virtualmachine *vm) {
-  printf("\nStack: ");
+void vm_dump(FILE *stream, const virtualmachine *vm) {
+  fprintf(stream, "\nStack: ");
   if (vm->stack_size > 0) {
     for (size_t i = 0; i < vm->stack_size; ++i) {
-      printf("  %ld", vm->stack[i]);
+      fprintf(stream, "  %ld", vm->stack[i]);
     }
   } else {
-    printf(" [empty]");
+    fprintf(stream, " [empty]");
   }
 }
 
 virtualmachine vm = {0};
+#define ARRAY_SIZE(xs) (sizeof(xs) / sizeof((xs)[0]))
+Inst program[] = {MAKE_INST_PUSH(69), MAKE_INST_PUSH(420), MAKE_INST_PLUS};
 
 int main() {
-  vm_execute_inst(&vm, inst_push(69));
-  vm_execute_inst(&vm, inst_push(420));
-  vm_execute_inst(&vm, inst_plus());
-  vm_dump(&vm);
+  vm_dump(stdout, &vm);
+  for (size_t i = 0; i < ARRAY_SIZE(program); i++) {
+    Trap trap = vm_execute_inst(&vm, program[i]);
+    if (trap != ERR_OK) {
+      fprintf(stderr, "Trap Activated %s\n", trap_as_cstr(trap));
+      vm_dump(stderr, &vm);
+      exit(1);
+    }
+  }
+  vm_dump(stdout, &vm);
   return 0;
 }
