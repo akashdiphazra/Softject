97c3767a7c452ca730e1946e7dc76ec0cafc446e
akashdiphazra <princehazra.96@gmail.com>
2023-01-14 21:25:10 +0530

    new instructions were added.

--- Virtual Machine Using C/vm.c
+++ Virtual Machine Using C/vm.c
@@ -2,13 +2,19 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define ARRAY_SIZE(xs) (sizeof(xs) / sizeof((xs)[0]))
+#define VM_PROGRAM_CAPACITY 1024
 #define VM_STACK_CAPACITY 1024
+#define VM_EXECUTION_LIMIT 69
 
 typedef enum {
   ERR_OK = 0,
   ERR_STACK_OVERFLOW,
   ERR_STACK_UNDERFOLOW,
   ERR_ILLEGAL_INSTRUCTION,
+  ERR_ILLEGAL_INST_ACCESS,
   ERR_DIV_BY_ZERO
 } Err;
 
@@ -22,6 +28,8 @@ const char *err_as_cstr(Err trap) {
     return "ERR_STACK_UNDERFOLOW";
   case ERR_ILLEGAL_INSTRUCTION:
     return "ERR_ILLEGAL_INSTRUCTION";
+  case ERR_ILLEGAL_INST_ACCESS:
+    return "ERR_ILLEGAL_INST_ACCESS";
   case ERR_DIV_BY_ZERO:
     return "ERR_DIV_BY_ZERO";
   default:
@@ -30,12 +38,19 @@ const char *err_as_cstr(Err trap) {
 }
 
 typedef int64_t Word;
-typedef struct {
-  Word stack[VM_STACK_CAPACITY];
-  size_t stack_size;
-} virtualmachine;
 
-typedef enum { INST_PUSH, INST_PLUS, INST_MINUS, INST_MUL, INST_DIV } Inst_Type;
+typedef enum {
+  INST_PUSH,
+  INST_PLUS,
+  INST_MINUS,
+  INST_MUL,
+  INST_DIV,
+  INST_JMP,
+  INST_JMPIF,
+  INST_EQ,
+  INST_HALT
+} Inst_Type;
+
 const char *inst_type_as_cstr(Inst_Type type) {
   switch (type) {
   case INST_PUSH:
@@ -48,6 +63,14 @@ const char *inst_type_as_cstr(Inst_Type type) {
     return "(INST_MUL)";
   case INST_DIV:
     return "(INST_DIV)";
+  case INST_JMP:
+    return "(INST_JMP)";
+  case INST_HALT:
+    return "(INST_HALT)";
+  case INST_JMPIF:
+    return "(INST_JMPIF)";
+  case INST_EQ:
+    return "(INST_EQ)";
   default:
     assert(0 && "inst_type_as_cstr: unreachable");
   }
@@ -57,6 +80,15 @@ typedef struct {
   Inst_Type type;
   Word operand;
 } Inst;
+
+typedef struct {
+  Word stack[VM_STACK_CAPACITY];
+  Word stack_size;
+  Inst program[VM_PROGRAM_CAPACITY];
+  Word program_size;
+  Word ip;
+  int halt;
+} virtualmachine;
 
 #define MAKE_INST_PUSH(value)                                                  \
   { .type = INST_PUSH, .operand = (value) }
@@ -68,39 +100,57 @@ typedef struct {
   { .type = INST_MUL }
 #define MAKE_INST_DIV                                                          \
   { .type = INST_DIV }
+#define MAKE_INST_JMP(addr)                                                    \
+  { .type = INST_JMP, .operand = (addr) }
+#define MAKE_INST_HALT(addr)                                                   \
+  { .type = INST_HALT, .operand = (addr) }
 
-Err vm_execute_inst(virtualmachine *vm, Inst inst) {
+Err vm_execute_inst(virtualmachine *vm) {
+  if (vm->ip < 0 || vm->ip >= vm->program_size) {
+    return ERR_ILLEGAL_INST_ACCESS;
+  }
+
+  Inst inst = vm->program[vm->ip];
+
   switch (inst.type) {
   case INST_PUSH:
     if (vm->stack_size >= VM_STACK_CAPACITY) {
       return ERR_STACK_UNDERFOLOW;
     }
     vm->stack[vm->stack_size++] = inst.operand;
+    vm->ip += 1;
     break;
+
   case INST_PLUS:
     if (vm->stack_size < 2) {
       return ERR_STACK_OVERFLOW;
     } else {
       vm->stack[vm->stack_size - 2] += vm->stack[vm->stack_size - 1];
       vm->stack_size -= 1;
+      vm->ip += 1;
     }
     break;
+
   case INST_MINUS:
     if (vm->stack_size < 2) {
       return ERR_STACK_OVERFLOW;
     } else {
       vm->stack[vm->stack_size - 2] -= vm->stack[vm->stack_size - 1];
       vm->stack_size -= 1;
+      vm->ip += 1;
     }
     break;
+
   case INST_MUL:
     if (vm->stack_size < 2) {
       return ERR_STACK_OVERFLOW;
     } else {
       vm->stack[vm->stack_size - 2] *= vm->stack[vm->stack_size - 1];
       vm->stack_size -= 1;
+      vm->ip += 1;
     }
     break;
+
   case INST_DIV:
     if (vm->stack_size < 2) {
       return ERR_STACK_OVERFLOW;
@@ -109,18 +159,50 @@ Err vm_execute_inst(virtualmachine *vm, Inst inst) {
     } else {
       vm->stack[vm->stack_size - 2] /= vm->stack[vm->stack_size - 1];
       vm->stack_size -= 1;
+      vm->ip += 1;
     }
     break;
+
+  case INST_JMP:
+    vm->ip = inst.operand;
+    break;
+
+  case INST_HALT:
+    vm->halt = 1;
+    break;
+
+  case INST_EQ:
+    if (vm->stack_size < 2) {
+      return ERR_STACK_OVERFLOW;
+    } else {
+      vm->stack[vm->stack_size - 2] =
+          vm->stack[vm->stack_size - 1] == vm->stack[vm->stack_size - 2];
+      vm->stack_size -= 1;
+      vm->ip += 1;
+    }
+    break;
+
+  case INST_JMPIF:
+    if (vm->stack_size < 1) {
+      return ERR_STACK_UNDERFOLOW;
+    } else if (vm->stack[vm->stack_size - 1]) {
+      vm->stack_size -= 1;
+      vm->ip = inst.operand;
+    } else {
+      vm->ip += 1;
+    }
+    break;
+
   default:
     return ERR_ILLEGAL_INSTRUCTION;
   }
   return ERR_OK;
 }
 
-void vm_dump(FILE *stream, const virtualmachine *vm) {
+void vm_dump_stack(FILE *stream, const virtualmachine *vm) {
   fprintf(stream, "\nStack: ");
   if (vm->stack_size > 0) {
-    for (size_t i = 0; i < vm->stack_size; ++i) {
+    for (Word i = 0; i < vm->stack_size; ++i) {
       fprintf(stream, " %ld ", vm->stack[i]);
     }
   } else {
@@ -128,21 +210,26 @@ void vm_dump(FILE *stream, const virtualmachine *vm) {
   }
 }
 
-#define ARRAY_SIZE(xs) (sizeof(xs) / sizeof((xs)[0]))
 virtualmachine vm = {0};
-Inst program[] = {MAKE_INST_PUSH(69), MAKE_INST_PUSH(420), MAKE_INST_PLUS,
-                  MAKE_INST_PUSH(42), MAKE_INST_MINUS,     MAKE_INST_PUSH(2),
-                  MAKE_INST_MUL,      MAKE_INST_PUSH(0),   MAKE_INST_DIV};
+Inst program[] = {MAKE_INST_PUSH(0), MAKE_INST_PUSH(1), MAKE_INST_PLUS,
+                  MAKE_INST_JMP(1)};
+
+void load_program_from_memory(virtualmachine *vm, Inst *program,
+                              size_t program_size) {
+  assert(program_size < VM_PROGRAM_CAPACITY);
+  memcpy(vm->program, program, sizeof(program[0]) * program_size);
+  vm->program_size = program_size;
+}
 
 int main() {
-  vm_dump(stdout, &vm);
-  for (size_t i = 0; i < ARRAY_SIZE(program); i++) {
-    printf("%s\n", inst_type_as_cstr(program[i].type));
-    Err err = vm_execute_inst(&vm, program[i]);
-    vm_dump(stdout, &vm);
+  load_program_from_memory(&vm, program, ARRAY_SIZE(program));
+  vm_dump_stack(stdout, &vm);
+  for (int i = 0; i < VM_EXECUTION_LIMIT && !vm.halt; i++) {
+    Err err = vm_execute_inst(&vm);
+    vm_dump_stack(stdout, &vm);
     if (err != ERR_OK) {
       fprintf(stderr, "Error %s\n", err_as_cstr(err));
-      vm_dump(stderr, &vm);
+      vm_dump_stack(stderr, &vm);
       exit(1);
     }
   }
