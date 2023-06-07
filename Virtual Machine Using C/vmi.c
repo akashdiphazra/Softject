5bb62edb0f191a695f010e1a3cae3ba9696f2bb5
akashdiphazra <princehazra.96@gmail.com>
2023-06-07 21:43:48 +0530

    new files were added

--- Virtual Machine Using C/vmi.c
+++ Virtual Machine Using C/vmi.c
@@ -5,15 +5,16 @@ int main(int argc, char **argv) {
     fprintf(stderr, "Usage: ./vmi <input.ebasm>\n");
     fprintf(stderr, "Error: expected input\n");
     exit(1);
-  }
+}
   vm_load_program_from_file(&vm, argv[1]);
   for (int i = 0; i < 69 && !vm.halt; ++i) {
     Err err = vm_execute_inst(&vm);
+    vm_dump_stack(stdout, &vm);
     if (err != ERR_OK) {
       fprintf(stderr, "Error %s\n", err_as_cstr(err));
       exit(1);
     }
   }
-  vm_dump_stack(stdout, &vm);
+  // vm_dump_stack(stdout, &vm);
   return 0;
 }
